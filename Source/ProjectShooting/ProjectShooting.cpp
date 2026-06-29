44cdb80a3c92bb1a7eb847bc30d48f497361dee9
Noewewwwew <dnjs4475@naver.com>
2026-06-30 02:37:38 +0900

    [init] 프로젝트 생성

---
+++ Source/ProjectShooting/ProjectShooting.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectShooting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectShooting, "ProjectShooting" );
+
+DEFINE_LOG_CATEGORY(LogProjectShooting)
---
+++ Source/ProjectShooting/ProjectShootingGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectShootingGameMode.h"
+
+AProjectShootingGameMode::AProjectShootingGameMode()
+{
+	// stub
+}
